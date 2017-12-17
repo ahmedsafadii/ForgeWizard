e71c5a50074d96a3d7eb2ecc77d7e80cdf32f2f1
Wolf Devil <ahmedsafadii@gmail.com>
2017-12-17 02:34:10 +0200

    init mobile project

---
+++ Mobile/Forge Wizard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Forge Wizard
+//
+//  Created by Ahmed Safadi on 12/17/17.
+//  Copyright © 2017 Ahmed Safadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Mobile/Pods/Target Support Files/Pods-Forge Wizard/Pods-Forge Wizard-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Forge_Wizard : NSObject
+@end
+@implementation PodsDummy_Pods_Forge_Wizard
+@end
